53166b89faa10f3b04241e7439dfcb68e2f0338e
ali111la <alina.sadykova2005@example.com>
2024-11-18 21:08:02 +0500

    1

---
+++ src/main/java/com/Alina/AlinaaproooApplication.java
@@ -0,0 +1,13 @@
+package com.Alina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlinaaproooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlinaaproooApplication.class, args);
+	}
+
+}
